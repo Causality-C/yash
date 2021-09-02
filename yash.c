b91309546b0dbb21e848d17bb0980b6d2459944d
Causality-C <themessenger012@gmail.com>
2021-09-02 11:12:09 -0500

    Divided up piping mechanism

--- yash.c
+++ yash.c
@@ -18,7 +18,6 @@
 #define ZERO 0
 #define DEBUG 0
 
-
 typedef struct process{
   char ** argv;
   char * input_redir_file;
@@ -26,17 +25,20 @@ typedef struct process{
   char * err_redir_file;
   char * token;
   int iarg;
-  int pipe;
 } Process;
 
-// TODO: return statement can determine error
-void parse_command(Process * process, char ** argv){
+/**
+ * Parse the command until the nearest pipe
+ * Return 0 if pipe not encountered
+ * 1 if the pipe has been encountered
+ * TODO: -1 if there has been a parsing error
+ * */
+int parse_command(Process * process, char ** argv){
   char * token = process->token;
   // Tokenize Strings
   while(token != NULL){
-
     if(DEBUG){
-      printf("%s %d\n",token, process->iarg);
+     printf("%s %d\n",token, process->iarg);
     }
     // STDOUT 
     if(strcmp(token, OUTPUT_REDIRECT) == ZERO){          
@@ -61,8 +63,8 @@ void parse_command(Process * process, char ** argv){
     }
     // Pipe
     else if(strcmp(token, PIPE) == ZERO){
-      process->pipe = process->iarg;
-      argv[process->iarg++] = NULL;
+      argv[process->iarg++] = NULL; // NULL Terminate the Array 
+      return 1; // Pipe detected: GTFO
     }
     else{
       argv[process->iarg] = malloc(sizeof(char) *strlen(token) + 1);
@@ -72,82 +74,81 @@ void parse_command(Process * process, char ** argv){
   }	
   // Null Terminate arg array
   argv[process->iarg] = NULL; 
+  return 0;
 }
-void execute_command(Process * c_redir, char** argv){
+
+void check_redirects(Process * process){
+  if(process->input_redir_file != NULL){
+    int redirect_fd = open(process->input_redir_file, O_RDONLY);
+    dup2(redirect_fd, STDIN_FILENO);
+    close(redirect_fd);
+  }
+  if(process->output_redir_file != NULL){
+    int redirect_fd = open(process->output_redir_file, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+    dup2(redirect_fd, STDOUT_FILENO);
+    close(redirect_fd);
+  }
+  if(process->err_redir_file != NULL){
+    int redirect_fd = open(process->err_redir_file, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+    dup2(redirect_fd, STDERR_FILENO);
+  }
+}
+
+void execute_command(Process * process, char** argv){
   // Create new process, execute command
   pid_t forked = fork();
   if(forked == ZERO){
     char * cmd = argv[0];
     
-    // Check File Redirects
-    if(c_redir->input_redir_file != NULL){
-      int redirect_fd = open(c_redir->input_redir_file, O_RDONLY);
-      dup2(redirect_fd, STDIN_FILENO);
-      close(redirect_fd);
-    }
-    if(c_redir->output_redir_file != NULL){
-      int redirect_fd = open(c_redir->output_redir_file, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
-      dup2(redirect_fd, STDOUT_FILENO);
-      close(redirect_fd);
-    }
-    if(c_redir->err_redir_file != NULL){
-      int redirect_fd = open(c_redir->err_redir_file, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
-      dup2(redirect_fd, STDERR_FILENO);
-    }
-    
-    // Pipe -> TODO: INCLUDE REDIRECTION FOR PIPES
-    if(c_redir->pipe != -1){
-      int fd[2];
-      if(pipe(fd) == -1){
-        exit(1);
-      }
-
-      char * rcmd = argv[c_redir->pipe + 1];
-      char ** rargv = &argv[c_redir->pipe + 1];
-
-      // Left Side
-      int p1 = fork();
-      if(p1 == 0){
-        dup2(fd[1], STDOUT_FILENO);
-        close(fd[0]);
-        close(fd[1]);
-        execvp(cmd,argv);
-      }
-
-      // Right Side
-      int p2 = fork();
-      if(p2 == 0){
-        dup2(fd[0], STDIN_FILENO);
-        close(fd[0]);
-        close(fd[1]);
-        execvp(rcmd, rargv);
-      }
-
-      // Close Pipe and Wait Until Child Processes Are Finished
-      close(fd[0]);
-      close(fd[1]);
-      waitpid(p1,NULL,0);
-      waitpid(p2,NULL,0);
-
-      if(DEBUG){
-        for(int i = 0; i< c_redir->pipe; i++){
-          printf("%s ", c_redir->argv[i]);
-        }
-        printf("\n");
-        for(int i = c_redir->pipe + 1; i< c_redir->iarg; i++){
-          printf("%s ", c_redir->argv[i]);
-        } 
-        printf("\n");
-      }
-    }
-    else{
-      execvp(cmd, argv);
-    }
+    // Sets file descriptors for redirects
+    check_redirects(process); 
+    execvp(cmd, argv);
 
     // Writes invalid cmd to stderr
-    if(c_redir->err_redir_file != NULL){
+    if(process->err_redir_file != NULL){
       perror(cmd);
     }
+    exit(1); // Exit out if execvp failed
+  }
+}
+void execute_command_with_pipe(Process * l_process, Process * r_process, char ** largv, char ** rargv){
+  pid_t forked = fork();
+  if(forked == ZERO){
+    char * l_cmd = largv[0];
+    char * r_cmd = rargv[0];
+
+    int fd[2];
+    if(pipe(fd) == -1){
+      exit(1);
+    }
+
+    // Left Side
+    int p1 = fork();
+    if(p1 == 0){
+      dup2(fd[1], STDOUT_FILENO);
+      close(fd[0]);
+      close(fd[1]);
+      check_redirects(l_process);
+      execvp(l_cmd, largv);
+    }
+
+    // Right Side
+    int p2 = fork();
+    if(p2 == 0){
+      dup2(fd[0], STDIN_FILENO);
+      close(fd[0]);
+      close(fd[1]);
+      check_redirects(r_process);
+      execvp(r_cmd, rargv);
+    }
+
+    // Close Pipe and Wait Until Child Processes Are Finished
+    close(fd[0]);
+    close(fd[1]);
+    waitpid(p1,NULL,0);
+    waitpid(p2,NULL,0);
+    
+    // Support Error Redirection
     exit(1); // Exit out if execvp failed
   }
 }
@@ -165,34 +166,56 @@ int main(){
 	else{
 		while(1){
 			char * line = readline(PROMPT);
-			char * dup = strdup(line);
-			char * token = strtok(dup, ONE_SPACE);
+			char * line_dup = strdup(line);
+			char * token = strtok(line_dup, ONE_SPACE);
+      char * line_dup2;
+      char * token2;
 
 			// Command Parsing
 			char * argv[10];
+      char * rargv[10];
 
 			// Todo: modify exit condition, free unused blocks
 			if(strcmp(line, "exit") == ZERO){
 				free(line);
-				free(dup);
+				free(line_dup);
 				exit(1);
 			}
 
       // Redirection, there can only be one of each => TODO: CHECK 
-      Process process = {argv,NULL,NULL,NULL,token,0,-1};
-      parse_command(&process,process.argv);
-      if(DEBUG){
-        printf("PIPE: %d\n", process.pipe);
+      Process process = {argv,NULL,NULL,NULL,token,0};
+      Process right_process;
+      int pipe_exists = parse_command(&process,process.argv);
+
+      if(pipe_exists){
+        // Create a new argument array
+        line_dup2 = strdup(line);
+        token2 = strtok(line_dup2, PIPE);
+        token2 = strtok(NULL,ONE_SPACE);
+        right_process = (Process) {rargv,NULL,NULL,NULL,token2,0}; 
+        parse_command(&right_process,right_process.argv); 
       }
-      execute_command(&process,process.argv);
+
+      if(pipe_exists){
+        execute_command_with_pipe(&process, &right_process, process.argv, right_process.argv);
+      }
+      else{
+        execute_command(&process,process.argv);
+      }
 
 			// Free  
 			free(line);
-			free(dup);
+			free(line_dup);
       for(int j = 0; j< process.iarg; j++){
         free(argv[j]);
       }
-			wait(NULL);
+      if(pipe_exists){
+        for(int j = 0; j< right_process.iarg; j++){
+          free(rargv[j]);
+        } 
+        free(line_dup2);
+      }
+  		wait(NULL);
 		}
 	}
 
